ddb36617c01818a913d93be25b3e995991936a1a
aimarapb <ainara.perea@live.u-tad.com>
2025-02-17 13:40:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -50,13 +50,4 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
  
-const server = app.listen(port, () => {
-  console.log(`Launching server on http://localhost:${port}`);
-});
- 
-// Manejo adecuado del cierre del servidor
-process.on('SIGTERM', () => {
-  server.close(() => {
-    console.log('Process terminated');
-  });
-});
+module.exports = app;
